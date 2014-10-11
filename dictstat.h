652d19d9b85985cf753a3c3e2d2ba703597adaa2
Nathan Miniovich <Ikensei@users.noreply.github.com>
2014-10-11 03:20:31 -0400

    Just have to make print function

--- dictstat.h
+++ dictstat.h
@@ -8,5 +8,6 @@ void readDict(FILE* dict_file);
 void scanData(FILE* data_file);
 int trieDFS(trieNode* scout,char* buffer, char** wordarr,int* index);
 int counter(char* fileBuffer, trieNode* scout,int** countarr, int* index);
+int prefixBot(trieNode* parent);
 
 #endif
