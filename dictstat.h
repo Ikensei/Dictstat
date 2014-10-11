5222769c33e52d987c8f8d9a2c0d2aea8c22f8c8
Nathan Miniovich <Ikensei@users.noreply.github.com>
2014-10-11 02:40:30 -0400

    fill out if statements in counter

--- dictstat.h
+++ dictstat.h
@@ -7,6 +7,6 @@ int readval;
 void readDict(FILE* dict_file);
 void scanData(FILE* data_file);
 int trieDFS(trieNode* scout,char* buffer, char** wordarr,int* index);
-int counter(FILE* data_file, trieNode* scout,int** countarr, int* index);
+int counter(char* fileBuffer, trieNode* scout,int** countarr, int* index);
 
 #endif
